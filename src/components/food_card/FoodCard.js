510b24244c76461363cf4599b9c5dac5f47055f0
Marcin Krzykowski <marcin.krzykowski.2018@mumail.ie>
2020-08-27 22:12:03 +0100

    Fixed up the design and placing of image

--- src/components/food_card/FoodCard.js
+++ src/components/food_card/FoodCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import NavBar from "../nav_bar/NavBar";
 import "./style.css";
-import sampleImg from "./index.jpg";
+import sampleImg from "./index.jpeg";
 
 export default class FoodCard extends React.Component {
     render = () => {
         return (
             <div className="food-card">
                 <NavBar />
-                <div className="food-card-content">
+                <div className="p-h-80 flex-x flex-y">
                     <img src={sampleImg} alt="a nice cuisine" />
                 </div>
             </div>
