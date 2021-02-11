4d552a49b796871c2cf7f75c782ab62a74a75440
InferNovac <45517444+InferNovac@users.noreply.github.com>
2021-02-11 16:14:13 +0000

    further refactoring

--- src/components/food_recipe/FoodRecipe.js
+++ src/components/food_recipe/FoodRecipe.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { removeRecipe } from '../redux/actions/RecipeAction';
-import ResponsiveSize from '../responsive_size/ResponsiveSize';
 import './food-recipe.css';
+
+const Recipe = (props) => {
+	const { recipe } = props;
+	return (
+		<div id='message-box-nav'>
+			<h4>{recipe.label}</h4>
+			<button className='send-btn' onClick={() => console.log('clicked')}>
+				UNMATCH
+			</button>
+			<Link to='/' className='recipe-btn'>
+				<span className='material-icons'>close</span>
+			</Link>
+		</div>
+	);
+};
 
 const Message = (props) => (
 	<div className='mt-1'>
@@ -16,20 +30,29 @@ const TextArea = () => {
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState([]);
 	return (
-		<ResponsiveSize lg>
+		<Fragment>
 			<div className='message-box'>{messages}</div>
 			<div className='message-box-input'>
-				<textarea
+				<input
 					name='message'
 					type='text'
 					placeholder={'Type in a message'}
 					className='p-h-100'
 					maxLength='5000'
 					onKeyDown={(event) => {
+						if (event.key === 'Enter') {
+							setMessages([
+								...messages,
+								<Message message={message} />,
+							]);
+							setMessage('');
+							event.preventDefault();
+							return;
+						}
 						setMessage(message + event.key);
 					}}
 					value={message}
-				></textarea>
+				/>
 				<button
 					className='send-btn'
 					onClick={() => {
@@ -43,28 +66,13 @@ const TextArea = () => {
 					SEND
 				</button>
 			</div>
-		</ResponsiveSize>
-	);
-};
-
-const Recipe = (props) => {
-	const { recipe } = props;
-	return (
-		<div className='message-box-nav'>
-			<h4>{recipe.label}</h4>
-			<button className='send-btn' onClick={() => console.log('clicked')}>
-				UNMATCH
-			</button>
-			<Link to='/' className='recipe-btn'>
-				<span className='material-icons'>close</span>
-			</Link>
-		</div>
+		</Fragment>
 	);
 };
 
 const FoodRecipe = (props) => {
 	return (
-		<div className='p-h-100 message-container'>
+		<div id='food-recipe'>
 			<Recipe recipe={props.recipe} />
 			<TextArea />
 		</div>
